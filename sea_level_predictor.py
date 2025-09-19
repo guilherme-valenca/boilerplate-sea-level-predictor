43461fdfa7ae97364fc1b8117c3f5c803ba90fec
guilherme-valenca <guilherme-valenca@sou.inteli.edu.br>
2025-09-19 18:06:48 -0300

    feat: completes the exercise

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,20 +4,29 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
 
     # Create first line of best fit
-
+    res = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    slope = res.slope
+    intercept = res.intercept
+    years_extended = pd.Series(range(df['Year'].min(), 2051))
+    plt.plot(years_extended, slope * years_extended + intercept, color='blue')
 
     # Create second line of best fit
-
+    df_recent = df[df['Year'] >= 2000]
+    res_recent = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
+    years_recent = pd.Series(range(2000, 2051))
+    plt.plot(years_recent, res_recent.slope * years_recent + res_recent.intercept, color='red')
 
     # Add labels and title
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
 
-    
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
